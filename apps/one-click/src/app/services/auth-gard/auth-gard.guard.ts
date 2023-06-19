b71622c13f6168ba6a819f253dbaa67aa1aafc9f
shivang-pokar <shivang.patel503@gmail.com>
2023-06-20 00:03:11 +0530

    push

--- apps/one-click/src/app/services/auth-gard/auth-gard.guard.ts
+++ apps/one-click/src/app/services/auth-gard/auth-gard.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService, CommonServiceService } from '@one-click/one-click-services';
+import { CookieService } from 'ngx-cookie-service';
 import { map, Observable, take, tap } from 'rxjs';
 
 
@@ -12,6 +13,7 @@ export class AuthGardGuard implements CanActivate {
 
   constructor(
     public router: Router,
+    private cookieService: CookieService,
     public commonServiceService: CommonServiceService,
     private angularFireAuth: AngularFireAuth,
   ) {
@@ -20,8 +22,9 @@ export class AuthGardGuard implements CanActivate {
 
   canActivate(): any {
     return new Promise(async (resolve, reject) => {
+      let company_id = this.cookieService.get('company_id')
       let user = await this.angularFireAuth.authState.pipe(take(1)).toPromise()
-      if (user?.uid) {
+      if (user?.uid && company_id) {
         resolve(true)
       } else {
         this.router.navigateByUrl('/login');
