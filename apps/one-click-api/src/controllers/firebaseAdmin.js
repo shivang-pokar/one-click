f6831f04978d6cf36a46e1dd68dbb7fbfddadb40
shivang-pokar <shivang.patel503@gmail.com>
2023-06-23 00:13:15 +0530

    Done

--- apps/one-click-api/src/controllers/firebaseAdmin.js
+++ apps/one-click-api/src/controllers/firebaseAdmin.js
@@ -1,9 +1,10 @@
 import * as admin from 'firebase-admin';
 import serviceAccount from '../one-click-desk-stage-firebase-adminsdk-wymg0-9ee2910ec1.json';
+import serviceAccountProd from '../oneclick-sch-firebase-adminsdk-vrx59-a40db4b4b6.json';
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://one-click-desk-stage-default-rtdb.firebaseio.com"
+    credential: admin.credential.cert((process.env.IS_PROD) ? serviceAccountProd : serviceAccount),
+    databaseURL: process.env.DATABASEURL
 });
 
 export default admin;
